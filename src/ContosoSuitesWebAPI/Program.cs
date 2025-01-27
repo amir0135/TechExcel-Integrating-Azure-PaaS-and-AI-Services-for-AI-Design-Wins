62d92a9d185764def8bfb0d4b03f1cba1bacf296
Amira Moussa <amiramoussa36@gmail.com>
2025-01-27 13:39:10 +0100

    updated program

--- src/ContosoSuitesWebAPI/Program.cs
+++ src/ContosoSuitesWebAPI/Program.cs
@@ -27,11 +27,9 @@ builder.Services.AddSingleton<IDatabaseService, DatabaseService>((_) =>
     return new DatabaseService(connectionString!);
 });
 
-
 // Create a single instance of the CosmosClient to be shared across the application.
 builder.Services.AddSingleton<CosmosClient>((_) =>
 {
-
     string userAssignedClientId = builder.Configuration["AZURE_CLIENT_ID"]!;
     var credential = new DefaultAzureCredential(
         new DefaultAzureCredentialOptions
@@ -67,7 +65,7 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 /**** Endpoints ****/
-// This endpoint serves as the default landing page for the API.
+// Default landing page for the API.
 app.MapGet("/", async () => 
 {
     return "Welcome to the Contoso Suites Web API!";
@@ -75,42 +73,47 @@ app.MapGet("/", async () =>
     .WithName("Index")
     .WithOpenApi();
 
+/****** HOTELS ENDPOINTS ******/
+
 // Retrieve the set of hotels from the database.
-app.MapGet("/Hotels", async () => 
+app.MapGet("/Hotels", async () =>
 {
-    throw new NotImplementedException();
+    var hotels = await app.Services.GetRequiredService<IDatabaseService>().GetHotels();
+    return hotels;
 })
     .WithName("GetHotels")
     .WithOpenApi();
 
 // Retrieve the bookings for a specific hotel.
-app.MapGet("/Hotels/{hotelId}/Bookings/", async (int hotelId) => 
+app.MapGet("/Hotels/{hotelId}/Bookings/", async (int hotelId) =>
 {
-    throw new NotImplementedException();
+    var bookings = await app.Services.GetRequiredService<IDatabaseService>().GetBookingsForHotel(hotelId);
+    return bookings;
 })
     .WithName("GetBookingsForHotel")
     .WithOpenApi();
 
 // Retrieve the bookings for a specific hotel that are after a specified date.
-app.MapGet("/Hotels/{hotelId}/Bookings/{min_date}", async (int hotelId, DateTime min_date) => 
+app.MapGet("/Hotels/{hotelId}/Bookings/{min_date}", async (int hotelId, DateTime min_date) =>
 {
-    throw new NotImplementedException();
+    var bookings = await app.Services.GetRequiredService<IDatabaseService>().GetBookingsByHotelAndMinimumDate(hotelId, min_date);
+    return bookings;
 })
     .WithName("GetRecentBookingsForHotel")
     .WithOpenApi();
+
+/****** OTHER ENDPOINTS ******/
 
 // This endpoint is used to send a message to the Azure OpenAI endpoint.
 app.MapPost("/Chat", async Task<string> (HttpRequest request) =>
 {
     var message = await Task.FromResult(request.Form["message"]);
-    
     return "This endpoint is not yet available.";
 })
     .WithName("Chat")
     .WithOpenApi();
 
 // This endpoint is used to vectorize a text string.
-// We will use this to generate embeddings for the maintenance request text.
 app.MapGet("/Vectorize", async (string text, [FromServices] IVectorizationService vectorizationService) =>
 {
     var embeddings = await vectorizationService.GetEmbeddings(text);
@@ -122,7 +125,7 @@ app.MapGet("/Vectorize", async (string text, [FromServices] IVectorizationServic
 // This endpoint is used to search for maintenance requests based on a vectorized query.
 app.MapPost("/VectorSearch", async ([FromBody] float[] queryVector, [FromServices] IVectorizationService vectorizationService, int max_results = 0, double minimum_similarity_score = 0.8) =>
 {
-    // Exercise 3 Task 3 TODO #3: Insert code to call the ExecuteVectorSearch function on the Vectorization Service. Don't forget to remove the NotImplementedException.
+    // Exercise 3 Task 3 TODO #3: Insert code to call ExecuteVectorSearch on the Vectorization Service.
     throw new NotImplementedException();
 })
     .WithName("VectorSearch")
@@ -131,7 +134,7 @@ app.MapPost("/VectorSearch", async ([FromBody] float[] queryVector, [FromService
 // This endpoint is used to send a message to the Maintenance Copilot.
 app.MapPost("/MaintenanceCopilotChat", async ([FromBody]string message, [FromServices] MaintenanceCopilot copilot) =>
 {
-    // Exercise 5 Task 2 TODO #10: Insert code to call the Chat function on the MaintenanceCopilot. Don't forget to remove the NotImplementedException.
+    // Exercise 5 Task 2 TODO #10: Insert code to call the Chat function on the MaintenanceCopilot.
     throw new NotImplementedException();
 })
     .WithName("Copilot")
