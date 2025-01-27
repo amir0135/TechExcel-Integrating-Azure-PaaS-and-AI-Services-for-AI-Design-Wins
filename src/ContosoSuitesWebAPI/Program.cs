ffb505a95075dc9fd28e051713b90a3d35da5e1e
Amira Moussa <amiramoussa36@gmail.com>
2025-01-27 13:50:52 +0100

    try again

--- src/ContosoSuitesWebAPI/Program.cs
+++ src/ContosoSuitesWebAPI/Program.cs
@@ -65,7 +65,7 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 /**** Endpoints ****/
-// Default landing page for the API.
+// This endpoint serves as the default landing page for the API.
 app.MapGet("/", async () => 
 {
     return "Welcome to the Contoso Suites Web API!";
